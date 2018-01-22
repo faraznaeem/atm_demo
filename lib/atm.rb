a4c725f2c2f2c9169da77cf8d832431e68407d42
faraznaeem <faraznaeem@gmail.com>
2018-01-22 15:30:56 +0100

    Add withdrawal method to ATM

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,4 +4,8 @@ attr_accessor :funds
   def initialize
     @funds = 1000
   end
+
+  def withdraw(amount)
+    @funds -= amount
+  end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -6,4 +6,9 @@ describe Atm do
     expect(subject.funds).to eq 1000
   end
 
+  it 'funds are reduced at withdrawal' do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
+  end
+
 end
